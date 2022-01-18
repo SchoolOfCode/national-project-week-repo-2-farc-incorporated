427e72f4f943abb216b97a63b78a3d7e52ac7086
drinobre <dri_nobre@hotmail.com>
2022-01-18 18:28:39 +0000

    Fix the addUserToTheTable missing params

--- routes/index.js
+++ routes/index.js
@@ -6,19 +6,19 @@ router.use(cors());
 import { getAllUsers, addUserToTable } from "../models/users.js";
 
 router
-	.route("/users")
-	.post(async (req, res) => {
-		const { fullname, nickname, city, hobbies } = req.body;
+  .route("/users")
+  .post(async (req, res) => {
+    const { fullname, nickname, city, hobbies } = req.body;
 
-		const result = await addUserToTable(req.body);
-		res.json({ success: true, message: "user added", payload: result });
-	})
-	.get(async (req, res) => {
-		const result = await getAllUsers();
-		res.json({
-			success: true,
-			message: "Here are the users: ",
-			payload: result,
-		});
-	});
+    const result = await addUserToTable(fullname, nickname, city, hobbies);
+    res.json({ success: true, message: "user added", payload: result });
+  })
+  .get(async (req, res) => {
+    const result = await getAllUsers();
+    res.json({
+      success: true,
+      message: "Here are the users: ",
+      payload: result,
+    });
+  });
 export default router;
