2900400ada0fd83a0ea8b94070bc3f8210f38c40
Rory Maguire <rorymaguire00@gmail.com>
2022-01-19 18:34:07 +0000

    fixing cors stuff

--- app.js
+++ app.js
@@ -10,20 +10,20 @@ app.use(cors());
 const port = normalizePort(process.env.PORT || "4444");
 app.set("port", port);
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+	console.log(`Listening on port ${port}`);
 });
 
 function normalizePort(val) {
-  var port = parseInt(val, 10);
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-  return false;
+	var port = parseInt(val, 10);
+	if (isNaN(port)) {
+		// named pipe
+		return val;
+	}
+	if (port >= 0) {
+		// port number
+		return port;
+	}
+	return false;
 }
 
 export default app;
--- routes/index.js
+++ routes/index.js
@@ -1,43 +1,42 @@
 import express from "express";
 const router = express.Router();
-import app from "../app.js";
-// import cors from "cors";
-// router.use(
-//   cors({
-//     origin: "https://bootcampersproject.herokuapp.com/",
-//   })
-// );
+import cors from "cors";
+router.use(
+	cors({
+		origin: "https://bootcampersproject.herokuapp.com/",
+	})
+);
 
 import {
-  getAllUsers,
-  addUserToTable,
-  deleteUserFromTable,
+	getAllUsers,
+	addUserToTable,
+	deleteUserFromTable,
 } from "../models/users.js";
 
 router
-  .route("/users")
-  .post(async (req, res) => {
-    const { fullname, nickname, city, hobbies } = req.body;
-    console.log(req.body);
+	.route("/users")
+	.post(async (req, res) => {
+		const { fullname, nickname, city, hobbies } = req.body;
+		console.log(req.body);
 
-    const result = await addUserToTable(fullname, nickname, city, hobbies);
-    res.json({ success: true, message: "user added", payload: result });
-  })
-  .get(async (req, res) => {
-    const result = await getAllUsers();
-    res.json({
-      success: true,
-      message: "Here are the users: ",
-      payload: result,
-    });
-  });
+		const result = await addUserToTable(fullname, nickname, city, hobbies);
+		res.json({ success: true, message: "user added", payload: result });
+	})
+	.get(async (req, res) => {
+		const result = await getAllUsers();
+		res.json({
+			success: true,
+			message: "Here are the users: ",
+			payload: result,
+		});
+	});
 router
-  .route("/users/:id")
+	.route("/users/:id")
 
-  .delete(async (req, res) => {
-    const { id } = req.params;
-    await deleteUserFromTable(id);
-    res.json({ success: true, message: "user has been deleted" });
-  });
+	.delete(async (req, res) => {
+		const { id } = req.params;
+		await deleteUserFromTable(id);
+		res.json({ success: true, message: "user has been deleted" });
+	});
 
 export default router;
